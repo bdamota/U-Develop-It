2b324bc06a25ce2a2c2c3f4a6e1e640bea8f187c
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-05 17:03:47 -0700

    modularized party routes

--- server.js
+++ server.js
@@ -1,62 +1,16 @@
 const express = require('express');
+const db = require('./db/database');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const inputCheck = require('./utils/inputCheck');
 
-const db = require('./db/database');
 const apiRoutes = require('./apiRoutes');
-app.use('/api', apiRoutes);
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-  app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-  });
-
-  app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.get(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: row
-      });
-    });
-  });
-
-  app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.run(sql, params, function(err, result) {
-      if (err) {
-        res.status(400).json({ error: res.message });
-        return;
-      }
-  
-      res.json({ message: 'successfully deleted', changes: this.changes });
-    });
-  });
 
 
 
